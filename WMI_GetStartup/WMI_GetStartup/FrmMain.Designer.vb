c224cdd21977c0f707375c77099edd16e9433f4d
Ice <rubble-ice@outlook.com>
2017-02-02 13:52:31 +0900

    get Startup

---
+++ WMI_GetStartup/WMI_GetStartup/FrmMain.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LV = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'LV
+        '
+        Me.LV.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LV.Location = New System.Drawing.Point(0, 0)
+        Me.LV.Name = "LV"
+        Me.LV.Size = New System.Drawing.Size(461, 435)
+        Me.LV.TabIndex = 0
+        Me.LV.UseCompatibleStateImageBehavior = False
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 435)
+        Me.Controls.Add(Me.LV)
+        Me.Name = "FrmMain"
+        Me.Text = "GetWmi"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents LV As ListView
+End Class
---
+++ WMI_GetStartup/WMI_GetStartup/FrmMain.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Management
+Imports System.IO
+Imports Shell32
+Public Class FrmMain
+    Const quote As String = """"
+    Private _AllUsersStartup As String = Environment.GetFolderPath(Environment.SpecialFolder.Startup) & "\"
+    Private _CurrentUserStartup As String = Environment.GetFolderPath(Environment.SpecialFolder.Programs) &
+        "\Startup\"
+    Sub New()
+
+        InitializeComponent()
+        LvSet()
+
+    End Sub
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim m As New ManagementClass("Win32_StartupCommand")
+        Dim mc As ManagementObjectCollection = m.GetInstances()
+        For Each x In mc
+            Dim s() As String = {x("Caption").ToString,
+                x("Command").ToString, GetPath(x("Command").ToString)}
+            LV.Items.Add(New ListViewItem(s))
+        Next
+    End Sub
+    Private Sub LvSet()
+        LV.View = View.Details
+        LV.FullRowSelect = True
+        LV.MultiSelect = False
+        LV.Columns.Add("Cpation", 100)
+        LV.Columns.Add("Command", 250)
+        LV.Columns.Add("Image Path", 250)
+    End Sub
+
+    Private Function GetPath(Command As String) As String
+        If (Command.IndexOf(".exe")) > 0 Then
+            Command = Command.Replace(quote, "")
+            Command = Command.Substring(0, Command.IndexOf(".exe") + 1)
+            Return Command + "exe"
+
+        ElseIf (Command.IndexOf(".lnk") > 0) Then
+
+            If (File.Exists(_AllUsersStartup & Command)) Then
+                Command = _AllUsersStartup & Command
+                Return GetShortcutPath(Command)
+            Else
+                Command = _CurrentUserStartup & "\" & Command
+                Return GetShortcutPath(Command)
+            End If
+
+        Else
+            Command = Command.Replace(quote, "")
+            Command = Command.Replace("/", "")
+            Command = Command.Replace("-", "")
+            Return Command
+        End If
+
+    End Function
+
+    Private Function GetShortcutPath(Command As String)
+        Dim pathOnly As String = System.IO.Path.GetDirectoryName(Command)
+        Dim fileNameOnly As String = System.IO.Path.GetFileName(Command)
+
+
+        Dim shell As New Shell()
+        Dim folder As Folder = shell.NameSpace(pathOnly)
+        Dim folderItem As FolderItem = folder.ParseName(fileNameOnly)
+        If (folderItem IsNot Nothing) Then
+            Dim link As ShellLinkObject = DirectCast(folderItem.GetLink, ShellLinkObject)
+            Return link.Path
+        End If
+        Return String.Empty
+    End Function
+End Class
